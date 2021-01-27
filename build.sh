68ecbab403ce6fc3b212157a273ffabcc198f3e3
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2021-01-27 16:49:48 -0300

    update build.sh with odc-stat-worker build

--- build.sh
+++ build.sh
@@ -17,8 +17,9 @@ ODC_BUILD_MODE=""
 ODC_TAG_PREFIX="bdc"
 ODC_TAG_VERSION="1.8"
 ODC_IMAGE_TAG="${ODC_TAG_PREFIX}/odc:${ODC_TAG_VERSION}"
-ODC_STATS_IMAGE_TAG="${ODC_TAG_PREFIX}/odc-stats:${ODC_TAG_VERSION}"
 
+ODC_STATS_IMAGE_TAG="${ODC_TAG_PREFIX}/odc-stats:${ODC_TAG_VERSION}"
+ODC_STATS_DASK_IMAGE_TAG="${ODC_TAG_PREFIX}/odc-stats-worker:${ODC_TAG_VERSION}"
 
 #
 # Build ODC image with all the dependencies already installed
@@ -26,6 +27,7 @@ ODC_STATS_IMAGE_TAG="${ODC_TAG_PREFIX}/odc-stats:${ODC_TAG_VERSION}"
 echo "Building image for ODC..."
 
 cp .datacube.conf docker/odc/
+cp .datacube.conf docker/odc-stats-dask-worker/
 
 cd docker/odc
 
@@ -45,7 +47,21 @@ cd docker/odc-stats
 
 docker build ${ODC_BUILD_MODE} \
              --build-arg BASE_IMAGE=${ODC_IMAGE_TAG} \
-             -t "${ODC_TAG_PREFIX}/odc-stats:${ODC_TAG_VERSION}" \
+             -t "${ODC_STATS_IMAGE_TAG}" \
              --file Dockerfile .
+
+cd ..
+
+#
+# Build Dask-Worker image for ODC-Stats with all the dependencies already installed
+#
+echo "Building Dask-Worker image for ODC-Stats"
+
+cd odc-stats-dask-worker/
+
+docker build ${ODC_BUILD_MODE} \
+             -t "${ODC_STATS_DASK_IMAGE_TAG}" \
+             --file Dockerfile .
+
 
 cd ../../
