97a58b9e71a9718c83bb6022715fcd6c0dd385b5
Phelps <phelpsmemo@gmail.com>
2021-06-15 16:36:45 -0300

    install hdmedians and add support to install other packages (close #6)

---
+++ docker/odc/extras/odc-utils.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This file is part of ODC Docker.
+# Copyright (C) 2021 INPE.
+#
+# ODC Docker is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+#
+
+pip install hdmedians \
+    geopandas \
+    matplotlib \
+    ipyleaflet \
+    rasterio \
+    fiona \
+    shapely \
+    seaborn \
+    rtree \
+    folium
